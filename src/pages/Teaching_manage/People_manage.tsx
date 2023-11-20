50f687f9538f68533201cbe7b914be725c2a8996
6ZLeo <136341102+6ZLeo@users.noreply.github.com>
2023-11-20 17:25:39 +0800

    Update People_manage.tsx

--- src/pages/Teaching_manage/People_manage.tsx
+++ src/pages/Teaching_manage/People_manage.tsx
@@ -1,17 +1,128 @@
-import { PageContainer } from "@ant-design/pro-components";
-import {Card} from 'antd';
-import React from "react";
+import React, { useState } from 'react';
+import { PageContainer } from '@ant-design/pro-components';
+import { Card, Button, Table, Modal, Checkbox, Form, Select } from 'antd';
 
-const People_manage: React.FC = () =>{
-  return (
-    <PageContainer>
-      <Card>
-        <p>
-          这是人员管理界面
-        </p>
-      </Card>
-    </PageContainer>
-  )
+const { Option } = Select;
+
+const People_manage: React.FC = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [selectedPerson, setSelectedPerson] = useState<any>(null);
+
+    const handleAdjust = (record) => {
+        setSelectedPerson(record);
+        setIsModalVisible(true);
+    };
+
+    const handleModalOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleModalCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    const students = [
+        {
+            key: '1',
+            name: '曾令腾',
+            student_id: '21301002',
+            courses: ['软件测试与质量保证', '专业课程综合实训III'],
+        },
+        {
+          key: '2',
+          name: '李明',
+          student_id: '21301000',
+          courses: ['机械制图', '高等数学'],
+        },
+    ];
+
+    const teachers = [
+        {
+            key: '1',
+            name: '完颜慧德',
+            teacher_id: 'T001',
+            courses: ['大学生心理健康', '闺蜜敌密辩证法'],
+        },
+    ];
+
+    const columns = [
+        {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'ID',
+            dataIndex: 'student_id',
+            key: 'student_id',
+        },
+        {
+            title: '课程',
+            key: 'courses',
+            render: (record: any) => (
+                <Button type="link" onClick={() => handleAdjust(record)}>
+                    调整
+                </Button>
+            ),
+        },
+    ];
+
+    const renderCourseOptions = () => {
+        // 根据实际情况生成课程选项
+        const courses = ['软件测试与质量保证', '专业课程综合实训III','机械制图', '高等数学','大学生心理健康', '闺蜜敌密辩证法'];
+        return courses.map((course) => (
+            <Option value={course} key={course}>
+                {course}
+            </Option>
+        ));
+    };
+
+    const renderCourseList = (person) => {
+        const { courses } = person;
+        return courses.map((course) => (
+            <Checkbox value={course} key={course}>
+                {course}
+            </Checkbox>
+        ));
+    };
+
+    return (
+        <PageContainer>
+            <Card>
+                <h2>学生列表</h2>
+                <Table dataSource={students} columns={columns} />
+
+                <h2>教师列表</h2>
+                <Table dataSource={teachers} columns={columns} />
+
+                <Modal
+                    title="调整课程"
+                    visible={isModalVisible}
+                    onOk={handleModalOk}
+                    onCancel={handleModalCancel}
+                >
+                    <Form>
+                        <Form.Item label="已选课程">
+                            <Checkbox.Group style={{ width: '100%' }}>
+                                {selectedPerson &&
+                                    selectedPerson.courses &&
+                                    renderCourseList(selectedPerson)}
+                            </Checkbox.Group>
+                        </Form.Item>
+                        <Form.Item label="全部课程">
+                            <Select
+                                mode="multiple"
+                                style={{ width: '100%' }}
+                                placeholder="请选择课程"
+                                defaultValue={selectedPerson?.courses}
+                            >
+                                {renderCourseOptions()}
+                            </Select>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+            </Card>
+        </PageContainer>
+    );
 };
-
 export default People_manage;
